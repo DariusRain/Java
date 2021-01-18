69dcfb74bbe7e668580c5b102f9c92ec163892f7
DariusRain <dariusrain.student@careerdevs.com>
2021-01-18 05:42:33 -0500

    Initial commit

---
+++ spring-rest-api-basic/src/test/java/com/coderain/springrestapibasic/SpringRestApiBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderain.springrestapibasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestApiBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
