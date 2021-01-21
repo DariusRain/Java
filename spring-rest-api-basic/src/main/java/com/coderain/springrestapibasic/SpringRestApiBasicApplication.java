09b1c923ab497af19025d945251ee16b16ba301a
DariusRain <dariusrain.student@careerdevs.com>
2021-01-21 15:04:40 -0500

    Adding to README.

--- spring-rest-api-basic/src/main/java/com/coderain/springrestapibasic/SpringRestApiBasicApplication.java
+++ spring-rest-api-basic/src/main/java/com/coderain/springrestapibasic/SpringRestApiBasicApplication.java
@@ -2,12 +2,19 @@ package com.coderain.springrestapibasic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringRestApiBasicApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringRestApiBasicApplication.class, args);
 	}
 
+	@GetMapping
+	public String test() {
+		return "Test Page";
+	}
 }
